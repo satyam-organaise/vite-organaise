356ffb429783ca56dd5b6ade729115532213b80e
satyam <satyamsonker1010@gmail.com>
2023-04-03 22:56:33 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { QueryClient, QueryClientProvider } from "react-query"
+import { Provider } from "react-redux"
+import store from "./Redux/Store/store";
+
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios';
+
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+})
+
+
+
+// if (process.env.NODE_ENV === 'development') {
+     axios.defaults.baseURL = "https://devorganaise.com/api";
+// } else if (process.env.NODE_ENV === 'production') {
+//   axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
+// }
+
+///// add  intercepter 
+axios.interceptors.request.use(
+  function (config) {
+    if ("userInfo" in localStorage) {
+      config.headers["Auth-Token"] = `Bearer ${(JSON.parse(localStorage.userInfo)).token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToastContainer autoClose={2000} />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
